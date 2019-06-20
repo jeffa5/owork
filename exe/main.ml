724f86776eb717efccdc6be7c4c931c32f587e29
Jeffas <dev@jeffas.io>
2019-06-20 12:15:16 +0100

    Rework executable
    This makes use of the new library and adds logging support along with
    the change of name.

--- exe/main.ml
+++ exe/main.ml
@@ -1,22 +1,22 @@
-open Cmdliner
-open Owork_timer.Timer
-
 (** Given the command line arguments create the config and start the server before handling the state *)
 let main work_duration short_break_duration long_break_duration
-    number_work_sessions notify_script =
-  print_endline "Starting timing server" ;
+    number_work_sessions notify_script socket_file style_renderer log_level =
+  Fmt_tty.setup_std_outputs ?style_renderer () ;
+  Logs.set_level log_level ;
+  Logs.set_reporter (Logs_fmt.reporter ()) ;
+  let open Owork in
   let config =
-    ref
-    @@ make_server_config
-         ~work_duration:(Duration.of_min work_duration)
-         ~short_break_duration:(Duration.of_min short_break_duration)
-         ~long_break_duration:(Duration.of_min long_break_duration)
-         ~number_work_sessions ~notify_script ()
+    Config.make
+      ~work_duration:(Duration.of_min work_duration)
+      ~short_break_duration:(Duration.of_min short_break_duration)
+      ~long_break_duration:(Duration.of_min long_break_duration)
+      ~number_work_sessions ~notify_script ~socket_file
   in
   Lwt_main.run
-    (let%lwt server = server config in
-     let _ = Lwt_unix.on_signal Sys.sigint (fun _ -> stop server) in
-     handle_state config)
+    (let%lwt () = Logs_lwt.debug (fun f -> f "%s" (Config.show config)) in
+     Server.run config)
+
+open Cmdliner
 
 let work_duration =
   let doc = "Length in minutes of the work session." in
@@ -35,16 +35,24 @@ let number_work_sessions =
   Arg.(value & opt int 3 & info ["n"; "work-sessions"] ~doc)
 
 let notify_script =
-  let doc = "Location of the script to handle the notifications." in
-  Arg.(value & opt (some file) None & info ["notify-script"] ~doc)
+  let doc =
+    "Location of the script to handle the notifications. If not specified \
+     then the server will try to use a default notification system."
+  in
+  Arg.(value & opt string "" & info ["notify-script"] ~doc)
+
+let socket_file =
+  let doc = "Location to use for socket file." in
+  Arg.(value & opt string "/tmp/owork.sock" & info ["socket-file"] ~doc)
 
 let program =
   Term.(
     const main $ work_duration $ short_break_duration $ long_break_duration
-    $ number_work_sessions $ notify_script)
+    $ number_work_sessions $ notify_script $ socket_file
+    $ Fmt_cli.style_renderer () $ Logs_cli.level ())
 
 let info =
   let doc = "A productivity timing server." in
-  Term.info "productivity-timer" ~doc ~exits:Term.default_exits
+  Term.info "owork" ~doc ~exits:Term.default_exits
 
 let () = Term.exit @@ Term.eval (program, info)
